a6bbe10844eb876acc2e4cedb810cb08a8fdfef6
bak-tanya <tatyanabak@mail.ru>
2019-07-04 04:19:37 +0700

    Сделана возможность заполнить и отправить форму создания участника

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var NUMBER_OF_WIZARDS = 4;
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 
 var WizardNames = {
   FIRST_NAMES: [
@@ -25,7 +27,7 @@ var WizardNames = {
   ]
 };
 
-var Colors = {
+var WizardColors = {
   COAT: [
     'rgb(101, 137, 164)',
     'rgb(241, 43, 107)',
@@ -40,17 +42,39 @@ var Colors = {
     'blue',
     'yellow',
     'green'
+  ],
+  FIREBALL: [
+    '#ee4830',
+    '#30a8ee',
+    '#5ce6c0',
+    '#e848d5',
+    '#e6e848'
   ]
 };
 
 var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+var userDialogOpen = document.querySelector('.setup-open');
+var userDialogClose = userDialog.querySelector('.setup-close');
+var userDialogForm = userDialog.querySelector('.setup-wizard-form');
 
-var similarListElement = userDialog.querySelector('.setup-similar-list');
+var userCoatColor = userDialogForm.querySelector('.setup-player')
+    .querySelector('.setup-wizard-appearance')
+    .querySelector('.setup-wizard')
+    .querySelector('.wizard-coat');
 
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item');
+var inputUserCoatColor = userDialogForm.elements['coat-color'];
+
+var userEyesColor = userDialogForm.querySelector('.setup-player')
+    .querySelector('.setup-wizard-appearance')
+    .querySelector('.setup-wizard')
+    .querySelector('.wizard-eyes');
+
+var inputUserEyesColor = userDialogForm.elements['eyes-color'];
+
+var userFireballColor = userDialogForm.querySelector('.setup-player')
+    .querySelector('.setup-fireball-wrap');
+
+var inputUserFireballColor = userDialogForm.elements['fireball-color'];
 
 /**
  * функция возвращает значение случайного элемента массива
@@ -60,6 +84,79 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
 var getRandomItemFromArray = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
+
+/**
+ * функция отвечает за закрытие всплывающего окна
+ */
+var closePopup = function () {
+  userDialog.classList.add('hidden');
+};
+
+/**
+ * функция отвечает за закрытие всплывающего окна при нажатии на Esc
+ * @param {Object} evt
+ */
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    if (document.activeElement.className !== 'setup-user-name') {
+      closePopup();
+    }
+  }
+};
+
+/**
+ * функция отвечает за открытие окна при нажатии Enter
+ * @param {Object} evt
+ */
+var openPopup = function () {
+  userDialog.classList.remove('hidden');
+
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+userDialogOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+userDialogOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+userDialogClose.addEventListener('click', function () {
+  closePopup();
+});
+
+userDialogClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 13) {
+    closePopup();
+  }
+});
+
+userCoatColor.addEventListener('click', function () {
+  var randomCoatColor = getRandomItemFromArray(WizardColors.COAT);
+  userCoatColor.style.fill = randomCoatColor;
+  inputUserCoatColor.value = randomCoatColor;
+});
+
+userEyesColor.addEventListener('click', function () {
+  var randomEyesColor = getRandomItemFromArray(WizardColors.EYES);
+  userEyesColor.style.fill = randomEyesColor;
+  inputUserEyesColor.value = randomEyesColor;
+});
+
+userFireballColor.addEventListener('click', function () {
+  var randomFireballColor = getRandomItemFromArray(WizardColors.FIREBALL);
+  userFireballColor.style.background = randomFireballColor;
+  inputUserFireballColor.value = randomFireballColor;
+});
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
 
 /**
  * функция создает массив объектов волшебников
@@ -84,7 +181,7 @@ var createWizards = function (wizardCount, wizardNames, wizardSurnames, wizardCo
   return wizards;
 };
 
-var wizards = createWizards(NUMBER_OF_WIZARDS, WizardNames.FIRST_NAMES, WizardNames.SURNAMES, Colors.COAT, Colors.EYES);
+var wizards = createWizards(NUMBER_OF_WIZARDS, WizardNames.FIRST_NAMES, WizardNames.SURNAMES, WizardColors.COAT, WizardColors.EYES);
 
 /**
  * создает новый DOM-элемент с волшебником и передает ему значения
